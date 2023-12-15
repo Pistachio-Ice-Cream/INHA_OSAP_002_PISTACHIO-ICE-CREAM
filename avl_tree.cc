892a4eede11e936ff4cc28075da076d16c99900a
rla1wo23@gmail.com <rla1wo23@gmail.com>
2023-12-15 17:58:46 +0900

    fix:지수님 요청에 따른, avl_tree.cc파일 작성 확장 가능성 및, 제너릭 타입이 아닌 함수들을 분리하기 위함

---
+++ avl_tree.cc
@@ -0,0 +1,9 @@
+/*
+Copyright (c) 2023 Pistachio-Ice-Cream.
+Distributed under the MIT License (http://opensource.org/licenses/MIT).
+Created At: 2023-11-12, Created By: {rla1wo23, rla1wo23@gmail.com}.
+*/
+#ifndef AVL_TREE_CC
+#define AVL_TREE_CC
+#include "avl_tree.h"
+#endif;

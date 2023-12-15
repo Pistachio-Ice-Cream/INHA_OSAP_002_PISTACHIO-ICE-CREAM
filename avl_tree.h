90a0231938be910ebb82567ba9f26be2a4c3d3e7
rla1wo23@gmail.com <rla1wo23@gmail.com>
2023-12-15 20:30:45 +0900

    Fix:파일명 변경 및, 사용하지 않는 submit.cc파일 제거 제출 조건을 만족하기 위한 제출입니다.

--- avl_tree.h
+++ avl_tree.h
@@ -49,13 +49,15 @@ class AVLTree {
       // iterator가 있어야 할 위치의 빈 공간을 찾았습니다.
       // 해당 위치에 새로운 노드를 추가합니다.
       TreeNode<value_type>* new_node = new TreeNode<value_type>;
-      this->node_counter_++;
+      this->node_counter_++; // 삽입을 하는 codeblock이므로,counter 증가
       iterator = new_node;
-      new_node->set_key(key_of_new_node);
+      new_node->set_key(key_of_new_node); // 새로운 노드의 키 설정
       return new_node;
-    } else if (iterator->key() < key_of_new_node) {
+    } else if (iterator->key() <
+               key_of_new_node) { // 새로 삽입되 key가, 기준점보다 크다면
+                                  // 우측으로 이동
       iterator->right_ = InsertNode(iterator->right_, key_of_new_node);
-    } else {
+    } else { // 새로 삽입되 key가, 기준점보다 작다면 좌측으로 이동
       iterator->left_ = InsertNode(iterator->left_, key_of_new_node);
     }
     // 새로운 노드를 삽입하는 재귀적 과정이 끝납니다.
@@ -76,18 +78,19 @@ class AVLTree {
         delete iterator;
         this->root_ = nullptr;
         this->node_counter_ = 0;
-        return nullptr;
+        return nullptr; // 삭제 수행
       }
       if (iterator->NoChild()) { // 자식 노드가 없는 경우
-        delete iterator;
-        node_counter_--;
+        delete iterator;         // 반복자 삭제를 통한 메모리 해제
+        node_counter_--;         // 개수가 하나 삭제
         return nullptr;
       } else if (!iterator->LeftIsNull() && // 왼쪽 자식 노드만 있는 경우
                  iterator->RightIsNull()) {
         if (this->root_ == iterator) {
-          root_ = iterator->left_;
+          root_ = iterator->left_; // root인 경우 root를 교환하는 작업 수행
         }
-        TreeNode<value_type>* successor = iterator->left_;
+        TreeNode<value_type>* successor =
+            iterator->left_; // successor 생성 후 리턴
         node_counter_--;
         delete iterator;
         return successor;
@@ -96,7 +99,8 @@ class AVLTree {
         if (this->root_ == iterator) {
           root_ = iterator->right_;
         }
-        TreeNode<value_type>* successor = iterator->right_;
+        TreeNode<value_type>* successor =
+            iterator->right_; // successor은 오른쪽 자식
         node_counter_--;
         delete iterator;
         return successor;
@@ -128,6 +132,8 @@ class AVLTree {
   }
   TreeNode<value_type>* FindNodePtr(value_type find_target) const {
     TreeNode<value_type>* iterator = root_;
+    // 반복자를 만들어서, nullptr가 되지 않도록 기준점보다 작다면 왼쪽으로,
+    // 크다면 오른쪽으로 반복자가 이동하게끔 합니다.
     while (iterator != nullptr && iterator->key() != find_target) {
       iterator =
           (find_target < iterator->key()) ? iterator->left_ : iterator->right_;
@@ -135,6 +141,7 @@ class AVLTree {
     return iterator;
   }
   TreeNode<value_type>* Minimum(value_type x) const {
+    // 특정 서브트리의 Minimum은 항상 왼쪽으로만 진행하면 됩니다.
     TreeNode<value_type>* iterator = FindNodePtr(x);
     while (!iterator->LeftIsNull()) {
       iterator = iterator->left_;
@@ -142,6 +149,7 @@ class AVLTree {
     return iterator;
   }
   TreeNode<value_type>* Maximum(value_type x) const {
+    // 특정 서브트리의 Maximum은 항상 오른쪽으로만 진행하면 됩니다.
     TreeNode<value_type>* iterator = FindNodePtr(x);
     while (!iterator->RightIsNull()) {
       iterator = iterator->right_;
@@ -149,26 +157,32 @@ class AVLTree {
     return iterator;
   }
   int Rank(TreeNode<value_type>* target_node, value_type x) {
+    // 타겟 노드가, 전체 노드들 중 몇 번째 Rank인지 재귀적으로 반환합니다.
     if (target_node == nullptr) {
       return 0;
     }
-    if (target_node->key() <= x) {
+    if (target_node->key() <= x) { // 나보다 작은 노드들만 탐색하면 됩니다
       if (target_node->LeftIsNull()) {
-        return 1 + Rank(target_node->right_, x);
+        return 1 + Rank(target_node->right_,
+                        x); // 재귀적으로, Rank의 우측도 탐색합니다.
       } else {
         return 1 + target_node->left_->size() + Rank(target_node->right_, x);
+        // left_의 size = target보다 작은 서브트리의 크기이므로 O(1)에
+        // 바로 계산 가능합니다.
       }
     } else {
       return Rank(target_node->left_, x);
     }
   }
   int NodeHeight(TreeNode<value_type>* target_node) const {
+    // 특정 Node의 Height를 안전하게 반환합니다.
     if (target_node == nullptr) {
       return -1;
     }
     return target_node->height();
   }
   int FindDepth(value_type find_target) const {
+    // Traversal을 통해 Depth를 계산합니다.
     TreeNode<value_type>* iterator = root_;
     int depth_counter = 0;
     while (iterator != nullptr && iterator->key() != find_target) {
@@ -181,23 +195,36 @@ class AVLTree {
 
  protected:
   int CalculateSize(TreeNode<value_type>* target_node) const {
+    // 자식 노드들로부터 target_node의 서브트리의 트리를 계산합니다.
     return 1 + (!target_node->LeftIsNull() ? target_node->left_->size() : 0) +
            (!target_node->RightIsNull() ? target_node->right_->size() : 0);
   }
   int CalculateHeight(TreeNode<value_type>* target_node) const {
+    //'트리'의 Height를 계산합니다.
     return 1 + (std::max(NodeHeight(target_node->left_),
                          NodeHeight(target_node->right_)));
   }
   int CalculateBalance(TreeNode<value_type>* target_node) const {
+    // 트리의 Balance Factor를 계산합니다.
     if (target_node == nullptr) {
       return 0;
     }
     return NodeHeight(target_node->left_) - NodeHeight(target_node->right_);
   };
   TreeNode<value_type>* LLRotation(TreeNode<value_type>*& old_axis) {
+    // L로테이션을 수행합니다. R케이스에서 수행할 수 있습니다.
+    /*
+  Left Rotation을 수행합니다.
+          old                         new
+          / \     Left Rotate         / \
+        new  C   ------------>       A  old
+        / \                             / \
+       A   B                           B   C
+ */
     if (old_axis->RightIsNull()) {
       return old_axis;
     }
+    // old축과 new축을 교환합니다.
     TreeNode<value_type>* new_axis = old_axis->right_;
     old_axis->right_ = new_axis->left_;
     new_axis->left_ = old_axis;
@@ -205,6 +232,7 @@ class AVLTree {
     if (old_axis == root_) {
       root_ = new_axis;
     }
+    // 축을 교환하고, 혹시 height가 변경됐다면 변경합니다.
     old_axis->set_height(CalculateHeight(old_axis));
     new_axis->set_height(CalculateHeight(new_axis));
 
@@ -215,6 +243,14 @@ class AVLTree {
   }
 
   TreeNode<value_type>* RRRotation(TreeNode<value_type>*& old_axis) {
+    /*기작은 LL과 비슷합니다.
+ Right Rotation
+         old                         new
+         / \     Right Rotate        / \
+        A  new   ------------>     old  C
+           / \                     / \
+          B   C                   A   B
+*/
     if (old_axis->LeftIsNull()) {
       return old_axis;
     }
@@ -270,6 +306,7 @@ class AVLTree {
   TreeNode<value_type>* root_;
 
   TreeNode<value_type>* CopyTree(const TreeNode<value_type>* node) {
+    // 복사생성자를 위한, node copy함수의 집합입니다.
     if (node == nullptr) {
       return nullptr;
     }
